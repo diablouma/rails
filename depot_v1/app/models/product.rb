8640f87523f9662f9da116a8116c150edacd10e9
nelson <mh732@ndeveloper.com>
2013-08-02 17:05:36 -0500

    first commit (cherry-picked from 96d1684)

--- depot_v1/app/models/product.rb
+++ depot_v1/app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
   validates :title, :description, :image_url,:presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  #estoy en la pagina 105
 end
